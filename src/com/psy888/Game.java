25a65aa98c33388003c34cb6b7cdfb4c1d551701
psy888 <tbilisi.zp.snab@gmail.com>
2019-11-10 13:00:53 +0200

    init

---
+++ src/com/psy888/Game.java
@@ -0,0 +1,27 @@
+package com.psy888;
+
+public class Game {
+    //Field UI
+    static final char CHAR_SHIP = 'o';
+    static final char CHAR_SEA = '.';
+    static final char CHAR_MISS = '-';
+    static final char CHAR_HIT = 'x';
+
+    //Game field size 10x10
+    static final int FIELD_SIZE = 10;
+
+    //Game field ARRAY
+    char[][] userField = new char[FIELD_SIZE][FIELD_SIZE];
+    char[][] compField = new char[FIELD_SIZE][FIELD_SIZE];
+
+
+    /**
+        print game fields to console
+     */
+    public void printField(){
+    }
+
+
+
+
+}
---
+++ src/com/psy888/Main.java
@@ -0,0 +1,8 @@
+package com.psy888;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
